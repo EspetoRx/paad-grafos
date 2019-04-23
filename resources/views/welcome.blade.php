229703fb38c458dd50d06e88dabb0076f3a93fb1
Lucas Carvalho <lcdo2395@gmail.com>
2019-04-23 04:02:41 -0300

    Second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="{{asset("css/sb-admin-2.min.css")}}">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset("vendor/fontawesome-free/css/all.css")}}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <!-- MEU CSS -->
     <link rel="stylesheet" href="{{asset("css/main.css")}}">
