683cf340223642f294c239a7357a8ee4fb9be565
Lucas Carvalho <lcdo2395@gmail.com>
2019-08-24 18:33:24 -0300

    Novo commit 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -171,12 +171,40 @@
                         }
                     });
             });
-            jQuery(window).load(function() {
-                /mobile/i.test(navigator.userAgent) && document.documentElement.scrollTop === 0 && !pageYOffset && !location.hash && setTimeout(function () {
-                    window.scrollTo(0, 1);
-                }, 1000);
-            });
         });  
+        (function( win ){
+            var doc = win.document;
+            
+            // If there's a hash, or addEventListener is undefined, stop here
+            if( !location.hash && win.addEventListener ){
+                
+                //scroll to 1
+                win.scrollTo( 0, 1 );
+                var scrollTop = 1,
+                    getScrollTop = function(){
+                        return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+                    },
+                
+                    //reset to 0 on bodyready, if needed
+                    bodycheck = setInterval(function(){
+                        if( doc.body ){
+                            clearInterval( bodycheck );
+                            scrollTop = getScrollTop();
+                            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+                        }	
+                    }, 15 );
+                
+                win.addEventListener( "load", function(){
+                    setTimeout(function(){
+                        //at load, if user hasn't scrolled more than 20 or so...
+                        if( getScrollTop() < 20 ){
+                            //reset to hide addr bar at onload
+                            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+                        }
+                    }, 0);
+                }, false );
+            }
+        })( this );
     </script>
     <div class="modal fade" id="confirm" tabindex="-1" role="dialog" aria-labelledby="confirm-label" aria-hidden="true">
         <div class="modal-dialog modal-sm">
