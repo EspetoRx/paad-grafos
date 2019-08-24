5fe72fb58f7083fe059698d683d4eacb6255b4e6
Lucas Carvalho <lcdo2395@gmail.com>
2019-08-24 18:37:06 -0300

    Novo commit 14

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -172,17 +172,17 @@
                     });
             });
         });  
-        (function( win ){
-            var doc = win.document;
+        (function( window ){
+            var doc = window.document;
             
             // If there's a hash, or addEventListener is undefined, stop here
-            if( !location.hash && win.addEventListener ){
+            if( !location.hash && window.addEventListener ){
                 
                 //scroll to 1
-                win.scrollTo( 0, 1 );
+                window.scrollTo( 0, 1 );
                 var scrollTop = 1,
                     getScrollTop = function(){
-                        return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+                        return window.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
                     },
                 
                     //reset to 0 on bodyready, if needed
@@ -190,16 +190,16 @@
                         if( doc.body ){
                             clearInterval( bodycheck );
                             scrollTop = getScrollTop();
-                            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+                            window.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
                         }	
                     }, 15 );
                 
-                win.addEventListener( "load", function(){
+                window.addEventListener( "load", function(){
                     setTimeout(function(){
                         //at load, if user hasn't scrolled more than 20 or so...
                         if( getScrollTop() < 20 ){
                             //reset to hide addr bar at onload
-                            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+                            window.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
                         }
                     }, 0);
                 }, false );
